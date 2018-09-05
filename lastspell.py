0821dd7d1baf346516e94aae617da1e8bb691dfd
deltacluse <deltacluse@naver.com>
2018-09-05 10:46:22 +0900

    .

--- lastspell.py
+++ lastspell.py
@@ -33,7 +33,7 @@ for t in last5:
     
     
 # Seperate test data
-batch_size = int(count_word * 0.1)
+batch_size = int(count_word * 0.05)
 randstart = random.randint(0, count_word-batch_size)
 test_input = np.array(origin_input[randstart:randstart+batch_size])
 test_target = np.array(origin_target[randstart:randstart+batch_size])
@@ -52,7 +52,7 @@ def next_batch(train_input, train_target):
 # Set options
 learning_rate = 0.01  # ?
 n_hidden = 128  # hidden layer's depth? 
-total_epoch = 500
+total_epoch = 100
 n_step = len(last5[0])  # input length
 # len(last5[0]) : 5
 
@@ -95,6 +95,7 @@ accuracy = tf.reduce_mean(tf.cast(prediction_check, tf.float32))
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
 
+    array_precision, array_recall = [], []
     for epoch in range(1, total_epoch+1):
         input_batch, target_batch = next_batch(train_input, train_target)
         _, loss = sess.run([optimizer, cost], feed_dict={X: input_batch, Y: target_batch})
@@ -118,22 +119,59 @@ with tf.Session() as sess:
             average_precision = average_precision_score(a, b)
             precision, recall, _ = precision_recall_curve(a, b)
 
-            print("\n\n precision, recall == ==========================")
-            print(precision)
-            print(recall)
+            _precision, _recall = check_pr.sehan_precision_recall(test_target, predict)
+            array_precision.append(_precision)
+            array_recall.append(_recall)
 
-            precision, recall = check_pr(a, b)
+# plt.step(array_recall, array_precision, color='b', alpha=0.2,
+#          where='post')
+# plt.fill_between(array_recall, array_precision, step='post', alpha=0.2,
+#                  color='b')
 
-plt.step(recall, precision, color='b', alpha=0.2,
-         where='post')
-plt.fill_between(recall, precision, step='post', alpha=0.2,
-                 color='b')
+# plt.xlabel('Recall')
+# plt.ylabel('Precision')
+# plt.ylim([0.0, 1.05])
+# plt.xlim([0.0, 1.0])
+# plt.title('2-class Precision-Recall curve: AP={0:0.2f}'.format(
+#           average_precision))
 
+from itertools import cycle
+# setup plot details
+colors = cycle(['navy', 'turquoise', 'darkorange', 'cornflowerblue', 'teal'])
+
+plt.figure(figsize=(7, 8))
+f_scores = np.linspace(0.2, 0.8, num=4)
+lines = []
+labels = []
+for f_score in f_scores:
+    x = np.linspace(0.01, 1)
+    y = f_score * x / (2 * x - f_score)
+    l, = plt.plot(x[y >= 0], y[y >= 0], color='gray', alpha=0.2)
+    plt.annotate('f1={0:0.1f}'.format(f_score), xy=(0.9, y[45] + 0.02))
+
+lines.append(l)
+labels.append('iso-f1 curves')
+l, = plt.plot(recall["micro"], precision["micro"], color='gold', lw=2)
+lines.append(l)
+labels.append('micro-average Precision-recall (area = {0:0.2f})'
+              ''.format(average_precision["micro"]))
+
+for i, color in zip(range(n_classes), colors):
+    l, = plt.plot(recall[i], precision[i], color=color, lw=2)
+    lines.append(l)
+    labels.append('Precision-recall for class {0} (area = {1:0.2f})'
+                  ''.format(i, average_precision[i]))
+
+fig = plt.gcf()
+fig.subplots_adjust(bottom=0.25)
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
 plt.xlabel('Recall')
 plt.ylabel('Precision')
-plt.ylim([0.0, 1.05])
-plt.xlim([0.0, 1.0])
-plt.title('2-class Precision-Recall curve: AP={0:0.2f}'.format(
-          average_precision))
+plt.title('Extension of Precision-Recall curve to multi-class')
+plt.legend(lines, labels, loc=(0, -.38), prop=dict(size=14))
+
 
 plt.savefig('test0.png')
+
+print("size : " + str(batch_size))
